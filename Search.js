cc24a7bb7e1933cb4d0782c77e3858f662aede7b
kgagan <87615380+kgagan@users.noreply.github.com>
2021-08-04 08:41:31 +0530

    Add files via upload

---
+++ Search.js
@@ -0,0 +1,98 @@
+import React from "react";
+import "./style.css";
+import Header from "./Home";
+import axios from "axios";
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      s: this.props.data,
+      search_data: [],
+    };
+  }
+  componentDidMount() {
+    //const data = this.state.s;
+    const key = this.props.match.params.url;
+    fetch("https://restcountries.eu/rest/v2/name/" + key).then((data) => {
+      data
+        .json()
+        .then((resp) => {
+          this.setState({ search_data: resp });
+        })
+        .catch((errors) => {
+          console.log(errors);
+        });
+    });
+    //const fd = new FormData();
+    // fd.append("name", data);
+    // axios
+    //   .post("https://restcountries.eu/rest/v2/", fd)
+    //   .then((res) => {
+    //     axios.get("https://restcountries.eu/rest/v2/").then((res) => {
+    //       this.setState({ search_data: res.data });
+    //     });
+    //   })
+    //   .catch((errors) => {
+    //     console.log(errors);
+    //   });
+  }
+
+  render() {
+    return (
+      <div>
+        {/* start here */}
+        <div className="main-div">
+          <div
+            className="container"
+            style={{
+              height: 500,
+              marginTop: 200,
+              backgroundColor: "grey",
+              textAlign: "center",
+            }}
+          >
+            <div className="row">
+              <div className="col-md-12">
+                <div className="contact-us">
+                  <h1 style={{ color: "white" }}>Country Details</h1>
+                </div>
+                <table class="table table-hover table-bordered">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>population</th>
+                      <th>latlng</th>
+                      <th>flag</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.search_data.map((result) => {
+                      return (
+                        <tr>
+                          <td>{result.name}</td>
+                          <td>{result.population}</td>
+                          <td>{result.latlng}</td>
+                          <td>
+                            <img
+                              style={{ width: "100px", height: "100px" }}
+                              src={result.flag}
+                              alt="no img"
+                            />
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end here */}
+      </div>
+    );
+  }
+}
+
+export default Search;
